fd526ed6d968eb5d8d431e8e8ade4c3a6a8f2cab
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-26 15:23:55 -0500

    Update trade_manager.py

--- src/executors/trade_manager.py
+++ src/executors/trade_manager.py
@@ -1,5 +1,20 @@
 from binance.client import Client
 import os
+from src.collectors.binance_client import client
+import yaml
+
+with open("config/settings.yaml") as f:
+    config = yaml.safe_load(f)
+
+def place_order(signal):
+    symbol = config["trade"]["symbol"]
+    quantity = config["trade"]["trade_amount"]
+
+    if signal == 1:
+        order = client.order_market_buy(symbol=symbol, quantity=quantity)
+        print(f"✅ Buy Order Executed: {order}")
+    else:
+        print("🟡 Hold / No trade executed.")
 
 client = Client(api_key=os.getenv("BINANCE_API_KEY"), api_secret=os.getenv("BINANCE_SECRET"))
 
