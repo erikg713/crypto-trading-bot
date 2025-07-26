c1c0d92f737262974c105fb7fb861c35f8f4caa2
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-26 15:06:29 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+import time
+from src.learners.predict_signals import get_latest_signal
+from src.executors.trade_manager import place_order
+
+while True:
+    signal, confidence = get_latest_signal()
+    if signal == 1 and confidence > 0.75:
+        place_order(signal)
+    else:
+        print(f"Hold — Confidence: {confidence:.2f}")
+    time.sleep(900)  # Run every 15 mins
 
