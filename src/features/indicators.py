1e6c33fb47dbdd2a3b08fb64301f5a5a7313fe76
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-26 15:21:03 -0500

    Update indicators.py

--- src/features/indicators.py
+++ src/features/indicators.py
@@ -1,1 +1,8 @@
+import ta
+
+def add_indicators(df):
+    df['rsi'] = ta.momentum.RSIIndicator(df['close']).rsi()
+    df['ema_10'] = ta.trend.EMAIndicator(df['close'], window=10).ema_indicator()
+    df['macd'] = ta.trend.MACD(df['close']).macd()
+    return df
 
