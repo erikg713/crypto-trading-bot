1e7b4f0d4696e15eeaa8a4bf385b1e422c7095e3
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-27 00:32:34 -0500

    Update bitcoin_model.py

--- src/ai_models/bitcoin_model.py
+++ src/ai_models/bitcoin_model.py
@@ -1,1 +1,9 @@
+def predict(df):
+    # Dummy strategy: buy if last close > previous close else sell
+    if len(df) < 2:
+        return 0
+    if df['close'].iloc[-1] > df['close'].iloc[-2]:
+        return 1  # buy
+    else:
+        return -1  # sell
 
