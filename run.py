4676351a8152358b36628690b2ac4c93f2422f91
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-27 01:13:18 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
+from src.main import trading_job
+from src.utils.scheduler import JobScheduler
+from src.db.db_utils import init_db
+
+if __name__ == "__main__":
+    init_db()
+    scheduler = JobScheduler()
+    scheduler.add_job(trading_job, every=15, unit='minutes', job_name='Multi-Asset Trading Job')
+    scheduler.run_forever()
 
