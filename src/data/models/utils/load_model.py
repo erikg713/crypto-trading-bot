11e436faec2b50ff868c57702330ce03cb1754cb
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-27 02:54:00 -0500

    Update load_model.py

--- src/data/models/utils/load_model.py
+++ src/data/models/utils/load_model.py
@@ -1,1 +1,9 @@
+import joblib
+import os
+
+def load_model(filename, directory="src/data/models/checkpoints/"):
+    full_path = os.path.join(directory, filename)
+    model = joblib.load(full_path)
+    print(f"📦 Model loaded from {full_path}")
+    return model
 
