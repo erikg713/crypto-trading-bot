6f62d728290bb2da6e9ded4a05bd85bbc21b77f1
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-27 00:47:16 -0500

    Update stocks_model.py

--- src/ai_models/stocks_model.py
+++ src/ai_models/stocks_model.py
@@ -1,1 +1,11 @@
+def predict(symbol, df):
+    """
+    Dummy model: buy if close last > previous; sell if opposite.
+    """
+    if len(df) < 2:
+        return 0
+    if df['close'].iloc[-1] > df['close'].iloc[-2]:
+        return 'buy'
+    else:
+        return 'sell'
 
