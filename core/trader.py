44fd17fccb2375b4ddf29935346cee37d15e6942
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-01 12:51:28 -0500

    Update trader.py

--- core/trader.py
+++ core/trader.py
@@ -4,18 +4,25 @@ from strategies.factory import get_strategy
 from core.data_streamer import DataStreamer
 from core.order_executor import OrderExecutor
 from risk.risk_manager import RiskManager
+from backtest.simulator import Backtester
 
 class Trader:
-    def __init__(self, exchange_name, strategy_name):
+    def __init__(self, exchange_name, strategy_name, backtest=False):
         self.exchange = get_exchange(exchange_name)
         self.strategy = get_strategy(strategy_name)
-        self.data_streamer = DataStreamer(self.exchange)
-        self.order_executor = OrderExecutor(self.exchange)
+        self.backtest = backtest
         self.risk_manager = RiskManager()
+        if not backtest:
+            self.data_streamer = DataStreamer(self.exchange)
+            self.order_executor = OrderExecutor(self.exchange)
 
     def run(self):
-        for data in self.data_streamer.stream():
-            signal = self.strategy.generate_signal(data)
-            risk_action = self.risk_manager.evaluate(signal, data)
-            if risk_action:
-                self.order_executor.execute(risk_action)
+        if self.backtest:
+            backtester = Backtester(self.strategy)
+            backtester.run()
+        else:
+            for data in self.data_streamer.stream():
+                signal = self.strategy.generate_signal(data)
+                risk_action = self.risk_manager.evaluate(signal, data)
+                if risk_action:
+                    self.order_executor.execute(risk_action)
