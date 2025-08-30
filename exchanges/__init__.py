168d2bf7899629e200aacfc308c331229f78e744
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-29 19:17:17 -0500

    Update __init__.py

--- exchanges/__init__.py
+++ exchanges/__init__.py
@@ -1,1 +1,19 @@
+"""
+Exchanges package for AI-CryptoTrader.
 
+This package contains exchange connectors for multiple platforms:
+- Binance
+- Coinbase
+- Alpaca (future)
+- Others (plug-in architecture)
+
+You can import exchanges like:
+
+from exchanges import BinanceExchange, CoinbaseExchange
+"""
+
+from .binance import BinanceExchange
+from .coinbase import CoinbaseExchange
+
+# Optional: add other exchange imports here in the future
+# from .alpaca import AlpacaExchange
